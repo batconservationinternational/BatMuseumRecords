03c2de6789a647da3d31d3e4d3c581beaf63e5ba
mdwhitby <michael.whitby@gmail.com>
2019-10-08 13:01:05 -0500

    remove status prints

--- src/render_Reports.R
+++ src/render_Reports.R
@@ -6,9 +6,12 @@ source(paste0(here::here(), "/src/process_GBIFdata.R"))
 
 
 
-for(thisSPP in c('ALLSPP', "LABO", 'LACI', 'LANO')){
+# for(thisSPP in c('ALLSPP', "LABO", 'LACI', 'LANO')){
 
-rmarkdown::render(paste0(here::here(), "/reports/MuseumRecords_report.Rmd"), 
+  
+for(thisSPP in c('LANO')){  
+  
+rmarkdown::render(paste0(here::here(), "/src/MuseumRecords_report.Rmd"), 
                   params=list(SPP = thisSPP), 
-                  output_file=paste0(here::here(), "/reports/", thisSPP, "_report.html"))
+                  output_file=paste0(here::here(), "/docs/", thisSPP, "_report.html"))
 }
