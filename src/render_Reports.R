c237aacb3045527ecfd4818af14f2ead9138e97c
mdwhitby <michael.whitby@gmail.com>
2019-10-08 12:26:39 -0500

    update report location

--- src/render_Reports.R
+++ src/render_Reports.R
@@ -3,14 +3,12 @@
 source(paste0(here::here(), "/src/get_GBIFdata.R"))
        
 source(paste0(here::here(), "/src/process_GBIFdata.R"))
-mySPP="LABO"
-
-# mySPP = c('ALLSPP', "LABO", 'LACI', 'LANO')
 
 
-for(thisSPP in mySPP){
 
-rmarkdown::render(paste0(here::here(), "/src/MuseumRecords_report.Rmd"), 
+for(thisSPP in c('ALLSPP', "LABO", 'LACI', 'LANO')){
+
+rmarkdown::render(paste0(here::here(), "/reports/MuseumRecords_report.Rmd"), 
                   params=list(SPP = thisSPP), 
                   output_file=paste0(here::here(), "/reports/", thisSPP, "_report.html"))
 }
